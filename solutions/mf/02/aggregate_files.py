09d11f0a8bc62f2ebd393541be9db2e48eb76a14
Matthew Franglen <matthew@brandwatch.com>
2018-11-23 14:36:50 +0000

    Make arguments for aggregator simpler

--- solutions/mf/02/aggregate_files.py
+++ solutions/mf/02/aggregate_files.py
@@ -8,59 +8,57 @@ import pandas
 
 
 def main():
-    """ Reads three files and aggregates them """
+    """ Reads a number of files and aggregates them """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--in-csv')
-    parser.add_argument('--in-json')
-    parser.add_argument('--in-pickle')
-    parser.add_argument('--in-excel')
-    parser.add_argument('--out', required=True)
+    parser.add_argument('input', nargs='+')
+    parser.add_argument('output')
     arguments = parser.parse_args()
 
-    frames = []
-    if arguments.in_csv is not None:
-        frames.append(pandas.read_csv(arguments.in_csv))
+    readers = {
+        'csv': read_csv,
+        'json': read_json,
+        'xlsx': read_excel,
+        'pickle': read_pickle,
+    }
 
-    if arguments.in_json is not None:
-        frames.append(pandas.read_json(arguments.in_json))
-
-    if arguments.in_pickle is not None:
-        with open(arguments.in_pickle, 'rb') as handle:
-            frames.append(pickle.load(handle))
-
-    if arguments.in_excel is not None:
-        frames.append(pandas.read_excel(arguments.in_excel))
-
-    assert frames, 'must provide at least one input file'
+    frames = [readers[get_extension(file)](file) for file in arguments.input]
 
     merged_data = frames[0]
     for current in frames[1:]:
         merged_data = merged_data.merge(current)
-    merged_data.to_json(arguments.out)
+
+    merged_data.to_json(arguments.output)
 
 
-def merge_all(*entries):
-    """ Merge all arguments together """
-    left = entries[0]
-    for right in entries[1:]:
-        left = merge(left, right)
-    return left
+def get_extension(file):
+    """ Get the extension of the file.
+        Will lower case the result for ease of comparison. """
+    try:
+        return file[file.rindex('.') + 1:].lower()
+    except ValueError:
+        raise ValueError(
+            f'Filename {file} has no extension - cannot infer reader')
 
 
-def merge(left, right):
-    """ Simple shallow merge of data structures.
-        Lists are concatenated.
-        Dictionaries are merged by key.
-        The left is preferred to the right. """
-    if isinstance(left, list):
-        assert isinstance(right, list)
-        return left + right
-    if isinstance(left, dict):
-        assert isinstance(right, dict)
-        combined = right.copy()
-        combined.update(left)
-        return combined
-    return left
+def read_csv(file):
+    """ Read a CSV file into a data frame """
+    return pandas.read_csv(file)
+
+
+def read_json(file):
+    """ Read a json file into a data frame """
+    return pandas.read_json(file)
+
+
+def read_excel(file):
+    """ Read an excel file into a data frame """
+    return pandas.read_excel(file)
+
+
+def read_pickle(file):
+    """ Read a pickle file into a python object """
+    with open(file, 'rb') as handle:
+        return pickle.load(handle)
 
 
 if __name__ == '__main__':
